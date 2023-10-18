e0a6b1199bed40ee10c3d703ce1ff5f0a56203ff
Zaid-R <81107281+Zaid-R@users.noreply.github.com>
2023-10-18 18:51:16 +0300

    Update show_widget.dart
    using consumer

--- lib/show_widget.dart
+++ lib/show_widget.dart
@@ -3,25 +3,35 @@ import 'package:flutter/material.dart';
 import 'package:my_version_of_multiprovider/models.dart';
 import 'package:provider/provider.dart';
 
-// ignore: must_be_immutable
 class ShowWidget extends StatelessWidget {
   final Color color;
   final bool isSeconds;
-  ShowWidget({
+  const ShowWidget({
     Key? key,
     required this.color,
     required this.isSeconds,
   }) : super(key: key);
-  //var provider;
   @override
   Widget build(BuildContext context) {
-    
-    final  provider = context.watch<String>();
+    return isSeconds
+        ? Consumer<Seconds>(
+            builder: (_, Seconds provider, __) {
+              return buildCustomContainer(provider.lastUpdate);
+            },
+          )
+        : Consumer<Minutes>(
+            builder: (_, Minutes provider, __) {
+              return buildCustomContainer(provider.lastUpdate);
+            },
+          );
+  }
+
+  Container buildCustomContainer(String value) {
     return Container(
       color: color,
       height: 100,
       alignment: Alignment.center,
-      child: Text(provider),
+      child: Text(value),
     );
   }
 }
